faa14afd58f68ec168bf0027c7578d191d1f8a9d
supunran <40440408+supunran@users.noreply.github.com>
2019-05-09 09:55:58 -0400

    Create cisco_configs.py

---
+++ cisco_configs.py
@@ -0,0 +1,32 @@
+# Importing necessary libraries
+import netmiko
+import getpass
+import paramiko
+
+# Opening text files, which include list of devices (a) and the commands (c) to run 
+f = open ("a", "r")
+devices = f.read().splitlines()
+f.close()
+h = open ("c", "r")
+commands = h.read().splitlines()
+
+# Prompting for username and password to log into the device
+ssh_user = input("Enter username: ")
+ssh_pw = input('SSH Password: ')
+
+# Login into the devices and running configuration commands
+for device in devices:
+    try:
+        ssh_session = netmiko.ConnectHandler(device_type='cisco_ios', host=device, username=ssh_user, password=ssh_pw)
+        print ("Login to " + device + " Successful")
+        for command in commands:
+            print(ssh_session.send_config_set(command) + "\n")
+        ssh_session.disconnect()
+        print("Successfully disconnected from " + device + "\n")
+
+    except (netmiko.ssh_exception.NetMikoTimeoutException,
+            netmiko.ssh_exception.NetMikoAuthenticationException,
+            paramiko.ssh_exception.SSHException) as s_error:
+        print(s_error)
+        
+# End of the script
